8e922ce678f76c3a60799066b95289bbd7b66d3d
Arnoud de Boer <a.deboer@geonovum.nl>
2023-05-03 16:36:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var respecConfig = {
-  specStatus: "GN-DEF",
+  specStatus: "GN-VV",
   specType: "IM", 
   publishDate: "2023-05-04",
   maxTocLevel: 4,
@@ -13,7 +13,7 @@ var respecConfig = {
       url: "https://www.geonovum.nl", 
     },  
   ],
-  shortName: "bro-gld",
+  shortName: "gld",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/GLD",
   previousPublishDate: "2023-03-16",
