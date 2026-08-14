b383c1ae03b1ecf655d85cc0eb0c08d6a257438b
theunissenr <renee.theunissen@tno.nl>
2026-08-14 16:08:20 +0200

    Config.js aangepast

--- js/config.js
+++ js/config.js
@@ -14,7 +14,7 @@ let respecConfig = {
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                 // Norm
   //specType: "ST",                   // Standaard
-  specType: "IM",                 // Informatie Model
+  specType: "ST",                 // Informatie Model
   //specType: "PR",                 // Praktijkrichtlijn
   //specType: "HR",                 // HandReiking
   //specType: "WA",                 // Werkafspraak
@@ -30,12 +30,12 @@ let respecConfig = {
   //-- shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
   shortName: "gld",
   //-- publishDate is verplicht -------------------------------------------------------
-  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
+  //-- De tag moet ingevuld zijn om de hyperlinks “Laatst gepubliceerde versie” en “Vorige versie” bovenaan de catalogus te tonen. Dit geldt ook voor een werkversie.
   publishDate: "2026-08-12",
   //custom header lines
-  //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
-  publishVersion: "1.2",
-  previousVersion: "1.1",
+  //publishVersion en previousVersion in de Werkversie invullen als “ “ (d.w.z. een spatie). De tag moet ingevuld zijn om de hyperlinks bovenaan de catalogus te tonen.
+  publishVersion: " ",
+  previousVersion: " ",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit!
   previousPublishDate: "2023-06-07",
   previousMaturity: "def",
